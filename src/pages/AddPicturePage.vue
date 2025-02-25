ac63d00f543edd92d81d10b6ef8cbbbe60e0c3bf
haoge <1668173696@qq.com>
2025-02-25 19:24:57 +0800

    图片编辑和AI扩图功能

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -4,19 +4,41 @@
       {{ route.query?.id ? '修改图片' : '创建图片' }}
     </h2>
     <a-typography-paragraph v-if="spaceId" type="secondary">
-      保持至空间： <a :href="`/space/${spaceId}`" target="_blank">{{spaceId}}</a>
+      保存至空间：<a :href="`/space/${spaceId}`" target="_blank">{{ spaceId }}</a>
     </a-typography-paragraph>
     <!-- 选择上传方式 -->
     <a-tabs v-model:activeKey="uploadType">
       <a-tab-pane key="file" tab="文件上传">
         <!-- 图片上传组件 -->
-        <PictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess"/>
+        <PictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess" />
       </a-tab-pane>
       <a-tab-pane key="url" tab="URL 上传" force-render>
         <!-- URL 图片上传组件 -->
-        <UrlPictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess"/>
+        <UrlPictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess" />
       </a-tab-pane>
     </a-tabs>
+    <!-- 图片编辑 AI 编辑 + 编辑图片-->
+    <div v-if="picture" class="edit-bar">
+      <a-space size="middle">
+        <a-button :icon="h(EditOutlined)" @click="doEditPicture">编辑图片</a-button>
+        <a-button type="primary" :icon="h(FullscreenOutlined)" @click="doImagePainting">
+          AI 扩图
+        </a-button>
+      </a-space>
+      <ImageCropper
+        ref="imageCropperRef"
+        :imageUrl="picture?.url"
+        :picture="picture"
+        :spaceId="spaceId"
+        :onSuccess="onCropSuccess"
+      />
+      <ImageOutPainting
+        ref="imageOutPaintingRef"
+        :picture="picture"
+        :spaceId="spaceId"
+        :onSuccess="onImageOutPaintingSuccess"
+      />
+    </div>
     <!-- 图片信息表单 -->
     <a-form
       v-if="picture"
@@ -26,7 +48,7 @@
       @finish="handleSubmit"
     >
       <a-form-item name="name" label="名称">
-        <a-input v-model:value="pictureForm.name" placeholder="请输入名称" allow-clear/>
+        <a-input v-model:value="pictureForm.name" placeholder="请输入名称" allow-clear />
       </a-form-item>
       <a-form-item name="introduction" label="简介">
         <a-textarea
@@ -62,27 +84,63 @@
 
 <script setup lang="ts">
 import PictureUpload from '@/components/PictureUpload.vue'
-import {computed, onMounted, reactive, ref} from 'vue'
-import {message} from 'ant-design-vue'
+import { computed, h, onMounted, reactive, ref } from 'vue'
+import { message } from 'ant-design-vue'
+import { EditOutlined, FullscreenOutlined } from '@ant-design/icons-vue'
 import {
   editPictureUsingPost,
   getPictureVoByIdUsingGet,
   listPictureTagCategoryUsingGet,
 } from '@/api/pictureController.ts'
-import {useRoute, useRouter} from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
+import ImageCropper from '@/components/ImageCropper.vue'
+import ImageOutPainting from "@/components/ImageOutPainting.vue";
 
-const router = useRouter()
-const route = useRoute()
+
 
 const picture = ref<API.PictureVO>()
 const pictureForm = reactive<API.PictureEditRequest>({})
 const uploadType = ref<'file' | 'url'>('file')
+// 图片编辑弹窗引用
+const imageCropperRef = ref()
+// AI 扩图弹窗引用
+const imageOutPaintingRef = ref()
+const categoryOptions = ref<string[]>([])
+const tagOptions = ref<string[]>([])
 
-//获取空间id
+const router = useRouter()
+
+// 空间 id
 const spaceId = computed(() => {
   return route.query?.spaceId
 })
+
+
+// AI 扩图
+const doImagePainting = () => {
+  if (imageOutPaintingRef.value) {
+    imageOutPaintingRef.value.openModal()
+  }
+}
+
+// 编辑成功事件
+const onImageOutPaintingSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+}
+
+
+// 编辑图片
+const doEditPicture = () => {
+  if (imageCropperRef.value) {
+    imageCropperRef.value.openModal()
+  }
+}
+
+// 编辑成功事件
+const onCropSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+}
 
 /**
  * 图片上传成功
@@ -92,7 +150,6 @@ const onSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture
   pictureForm.name = newPicture.name
 }
-
 
 /**
  * 提交表单
@@ -121,9 +178,6 @@ const handleSubmit = async (values: any) => {
   }
 }
 
-const categoryOptions = ref<string[]>([])
-const tagOptions = ref<string[]>([])
-
 /**
  * 获取标签和分类选项
  * @param values
@@ -148,10 +202,7 @@ const getTagCategoryOptions = async () => {
   }
 }
 
-onMounted(() => {
-  getTagCategoryOptions()
-})
-
+const route = useRoute()
 
 // 获取老数据
 const getOldPicture = async () => {
@@ -174,6 +225,7 @@ const getOldPicture = async () => {
 
 onMounted(() => {
   getOldPicture()
+  getTagCategoryOptions()
 })
 </script>
 
@@ -182,4 +234,11 @@ onMounted(() => {
   max-width: 720px;
   margin: 0 auto;
 }
+
+#addPicturePage .edit-bar {
+  text-align: center;
+  margin: 16px 0;
+}
+
 </style>
+
